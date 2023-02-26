b371b2e92c69e9973dd640fc1276d6c238e1ee7b
Suhas.m.malagihal <115938641+suhasm-18@users.noreply.github.com>
2023-02-26 11:09:20 +0530

    Create Arrays02.cpp

---
+++ Arrays02.cpp
@@ -0,0 +1,42 @@
+By using binarry search
+when you want to find a number in a array
+
+
+#include <iostream>
+using namespace std;
+
+int binnary(int arr[],int n, int key){
+    int s=0;
+    int e=n;
+    while(s<=e){
+        int mid=(s+e)/2;
+        if(arr[mid]==key){
+            return mid;
+        }
+        else if(arr[mid]>key){
+            e=mid-1;
+        }
+        else{
+            s=mid+1;
+        }
+    }
+    return -1;
+}
+
+int main() {
+   int n;
+   cin>>n;
+   
+   int arr[n];
+   
+   for(int i=0;i<n;i++){
+       cin>>arr[i];
+   }
+  int key;
+  cin>> key;
+  
+  cout<<binnary(arr,n,key);
+  
+   return 0;
+   
+}
