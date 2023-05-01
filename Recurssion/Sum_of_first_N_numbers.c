ed44b5526d461924995f5633c401827e91b75c4c
Suhas.m.malagihal <115938641+suhasm-18@users.noreply.github.com>
2023-05-01 20:13:50 +0530

    Create Sum_of_first_N_numbers.c

---
+++ Recurssion/Sum_of_first_N_numbers.c
@@ -0,0 +1,15 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+int sum(int n){
+    if(n==0)
+        return 0;
+        return sum(n-1)+n;
+}
+
+int main() {
+    int r;
+    r = sum(5);
+    printf("%d" , r);
+
+    return 0;
+}
