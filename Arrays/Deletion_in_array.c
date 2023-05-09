b66e12b45faf77c221c6a28f3f82855f6ea96d51
Suhas.m.malagihal <115938641+suhasm-18@users.noreply.github.com>
2023-05-09 21:45:29 +0530

    Update Deletion_in_array.c

--- Arrays/Deletion_in_array.c
+++ Arrays/Deletion_in_array.c
@@ -12,3 +12,17 @@ struct Array
  for(i=0;i<arr.length;i++)
  printf("%d ",arr.A[i]);
  }
+int Delete(struct Array *arr,int index)
+{
+ int x=0;
+ int i;
+ if(index>=0 && index<arr->length)
+ {
+ x=arr->A[index];
+ for(i=index;i<arr->length-1;i++)
+ arr->A[i]=arr->A[i+1];
+ arr->length--;
+ return x;
+ }
+ return 0;
+}
