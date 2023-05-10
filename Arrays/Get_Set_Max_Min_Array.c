c09ed12c6a05cd597992d100666d2240e6695fb9
Suhas.m.malagihal <115938641+suhasm-18@users.noreply.github.com>
2023-05-10 18:13:00 +0530

    Update Get_Set_Max_Min_Array.c

--- Arrays/Get_Set_Max_Min_Array.c
+++ Arrays/Get_Set_Max_Min_Array.c
@@ -18,3 +18,25 @@ void swap(int *x,int *y)
  *x=*y;
  *y=temp;
  }
+int Get(struct Array arr,int index)
+{
+ if(index>=0 && index<arr.length)
+ return arr.A[index];
+ return -1;
+}
+void Set(struct Array *arr,int index,int x)
+{
+ if(index>=0 && index<arr->length)
+ arr->A[index]=x;
+}
+int Max(struct Array arr)
+{
+ int max=arr.A[0];
+ int i;
+ for(i=1;i<arr.length;i++)
+ {
+ if(arr.A[i]>max)
+ max=arr.A[i];
+ }
+ return max;
+}
